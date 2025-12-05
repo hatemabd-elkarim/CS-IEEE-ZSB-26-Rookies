2cb9cb2bc61cad49c98dcbee2c122c695af2d179
Hatem Ayman <hatem.ayman.508@gmail.com>
2025-12-05 02:28:05 +0200

    Problem D

---
+++ Task-1/D.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<int> s;
+    int n, x;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        s.insert(x);
+    }
+
+    cout << s.size() << endl;
+}
